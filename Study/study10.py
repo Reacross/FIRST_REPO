280b64d22c3b6948b61d28069ee5a1794f954c6b
Reacross <145491534+Reacross@users.noreply.github.com>
2023-12-26 16:01:29 +0200

    Delete Study directory

--- Study/study10.py
+++
@@ -1,3 +0,0 @@
-UKRAINIAN_SYMBOLS = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
-TRANSLATION = ("a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
-               "f", "h", "ts", "ch", "sh", "sch", "", "ju", "ja")
